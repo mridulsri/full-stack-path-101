f27808923975ffa4313d110a661985c66cf56362
Mridul Srivastava <mridul.srivatsava.ctr@sabre.com>
2025-06-19 22:29:53 +0530

    Added JsonSchema project

---
+++ dot-net-core-c#/RandD_Solution/Concepts_101/PatternMatching.cs
@@ -0,0 +1,44 @@
+﻿namespace Concepts_101;
+
+public class PatternMatching
+{
+    public void CheckPatternMatching()
+    {
+        var apSettings = new ApSettings()
+        {
+            Tenant = "Mridul"
+        };
+        try
+        {
+            var someFeature = apSettings.SomeFeature;
+            if (someFeature.Enable)
+            {
+                Console.WriteLine("feature is enabled");
+            }
+            Console.WriteLine("feature is not enabled");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            throw;
+        }
+        
+
+    }
+
+    class ApSettings
+    {
+        public bool Enable { get; set; }
+        public string Tenant { get; set; }
+
+        public SomeFeature SomeFeature { get; set; }
+    }
+
+    class SomeFeature
+    {
+        public bool Enable { get; set; }
+        public IList<string> Code { get; set; }
+    }
+    
+    
+}
---
+++ dot-net-core-c#/RandD_Solution/Concepts_101/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Concepts_101;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        var list = new List<string>();
+        list = null;
+        var result = list?.Any()?? true;
+        
+        var obj = new PatternMatching();
+        obj.CheckPatternMatching();
+    }
+
+    class ApSettings
+    {
+        public bool Enable { get; set; }
+        public int SeatNumber { get; set; }
+        public string UserName { get; set; }
+        public Gender Gender { get; set; }
+        public Address Address { get; set; }
+    }
+
+    class Address
+    {
+        public bool Enable { get; set; }
+        public int Number { get; set; }
+        public string AddressLine { get; set; }
+        public PhoneType ContactNumber { get; set; }
+    }
+
+    private enum PhoneType
+    {
+        Teliphone,
+        Mobile
+        
+        
+    }
+    private enum Gender
+    {
+        Male,
+        Female,
+        Others
+    }
+}
