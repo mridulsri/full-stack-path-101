fabe5bb683d40abe1c14ef5a18fa6dac24b8e2b7
Mridul Srivastava <p3mtech@gmail.com>
2025-06-20 01:42:02 +0530

    Added optimize logic of xpath

--- dot-net-core-c#/DOMTrace/Scripts/highlight-tracker.js
+++ dot-net-core-c#/DOMTrace/Scripts/highlight-tracker.js
@@ -8,18 +8,32 @@ function highlightElement(el) {
     el.style.outline = '2px solid yellow';
 }
 
-function getXPath(element) {
-    if (element.id !== '') return 'id(\"' + element.id + '\")';
-    if (element === document.body) return element.tagName;
-
-    var ix = 0;
-    var siblings = element.parentNode.childNodes;
-    for (var i = 0; i < siblings.length; i++) {
-        var sibling = siblings[i];
-        if (sibling === element) return getXPath(element.parentNode) + '/' + element.tagName + '[' + (ix + 1) + ']';
-        if (sibling.nodeType === 1 && sibling.tagName === element.tagName) ix++;
-    }
-}
+function getXPath(elm) {
+    var allNodes = document.getElementsByTagName('*');
+    for (var segs = []; elm && elm.nodeType == 1; elm = elm.parentNode) {
+        if (elm.hasAttribute('id')) {
+            var uniqueIdCount = 0;
+            for (var n = 0; n < allNodes.length; n++) {
+                if (allNodes[n].hasAttribute('id') && allNodes[n].id == elm.id) uniqueIdCount++;
+                if (uniqueIdCount > 1) break;
+            };
+            if (uniqueIdCount == 1) {
+                segs.unshift('id("' + elm.getAttribute('id') + '")');
+                return segs.join('/');
+            } else {
+                segs.unshift(elm.localName.toLowerCase() + '[@id="' + elm.getAttribute('id') + '"]');
+            }
+        } else if (elm.hasAttribute('class')) {
+            segs.unshift(elm.localName.toLowerCase() + '[@class="' + elm.getAttribute('class') + '"]');
+        } else {
+            for (i = 1, sib = elm.previousSibling; sib; sib = sib.previousSibling) {
+                if (sib.localName == elm.localName) i++;
+            };
+            segs.unshift(elm.localName.toLowerCase() + '[' + i + ']');
+        };
+    };
+    return segs.length ? '/' + segs.join('/') : null;
+};
 
 document.addEventListener('contextmenu', function (e) {
     e.preventDefault();
