8e7bf09fefb252452432bd204f7f7cdc418928e6
Mridul Srivastava <mridul.srivatsava.ctr@sabre.com>
2025-06-19 22:28:30 +0530

    Added Dotnet project

---
+++ architect-ecosystem-101/patterns/event-driven/PubSub_Patterns/MessageBroker/RabbitMQBroker/RabbitMQSetting.cs
@@ -0,0 +1,20 @@
+﻿namespace MessageBroker.RabbitMQBroker;
+
+public class RabbitMQSetting
+{
+    public string? HostName { get; set; }
+    public string? UserName { get; set; }
+    public string? Password { get; set; }
+}
+
+//RabbitMQ Queue name
+public static class RabbitMQQueues
+{
+    public const string EmailQueue = "emailQueue";
+    public const string SmsQueue = "smsQueue";
+    public const string WhatsAppQueue = "whatsAppQueue";
+
+    public const string OrderQueue = "orderQueue";
+    public const string ChefQueue = "chefQueue";
+    public const string PickupQueue = "pickupQueue";
+}
